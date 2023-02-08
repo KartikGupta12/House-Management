640b8e80bad22d73d712216349e03775f48ce09d
Chirag Kalwani <75169388+chirag-kalwani@users.noreply.github.com>
2023-02-08 21:57:57 +0530

    Profile Menu added

---
+++ src/components/ProfileMenu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import profilePic from "../assets/profilePic.png";
+
+function ProfileMenu(props) {
+    return (
+        <div className="dropdown-center" data-bs-theme="dark">
+            <button className=" btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown">
+                <img width="30" className="mx-2" src={profilePic} alt="logo"/>
+                <span className="text-light user-select-none" role="button">Chirag_kalwani</span>
+            </button>
+            <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Settings</a></li>
+                <li><a className="dropdown-item" href="#">Logout</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default ProfileMenu;
